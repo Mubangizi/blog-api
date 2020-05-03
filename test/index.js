2b017f6edfca81b2429c8c9b553f237ef69a7e74
Mubangizi <mubangizia22@gmail.com>
2020-05-04 02:18:15 +0300

    added test db connection test

--- test/index.js
+++ test/index.js
@@ -4,6 +4,7 @@ server = require('../server'),
 should = chai.should();
 
 chai.use(chaiHttp);
+
  
 describe('Init', function () {
   it('check app status', function (done) {
@@ -12,5 +13,23 @@ describe('Init', function () {
           res.should.have.status(200);
           done();
       })
+  });
+});
+
+describe('test database', function () {
+  it('connect to database', function (done) {
+    const mysql = require("mysql");
+    const databaseConnetion = mysql.createConnection({
+      user: process.env.DBUSER,
+      host:  process.env.DBHOST,
+      database:  process.env.DBDATABASE,
+      password:  process.env.DBPASSWORD,
+    });
+
+    databaseConnetion.connect(error =>{
+      should.not.exist(error);
+      done();
+    })
+    
   });
 });
